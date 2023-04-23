f7f787ebe9127fa54529633f5155b873f56ac16c
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2023-04-23 20:39:37 +0900

    Pagination with Eloquent

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
     {
         $users = auth()->user()->following()->pluck("profiles.user_id");
 
-        $posts = Post::whereIn("user_id", $users)->orderBy("created_at", "desc")->get();
+        $posts = Post::whereIn("user_id", $users)->orderBy("created_at", "desc")->paginate(5);
 
         return view("posts/index", compact("posts"));
     }
